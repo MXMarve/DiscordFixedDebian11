6967d7bae343d7ee90ea79e2c51f85196e07ddd2
MXMarve <95297108+MXMarve@users.noreply.github.com>
2022-01-26 12:50:35 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,5 @@
+wget http://ftp.mx.debian.org/debian/pool/main/liba/libappindicator/libappindicator3-1_0.4.92-7_amd64.deb
+sudo apt install ./libappindicator3-1_*_amd64.debian -y
+wget http://ftp.mx.debian.org/debian/pool/main/libi/libindicator/libindicator3-7_0.5.0-3+b1_amd64.deb
+sudo apt install ./libindicator3-7_*_amd64.deb -y
+sudo apt install ./discord-fixed.deb -y
